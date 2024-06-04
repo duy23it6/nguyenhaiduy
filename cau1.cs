0aad79b2187a82a185bd77467af413bc56075ba8
duy23it6 <148861035+duy23it6@users.noreply.github.com>
2024-06-04 18:11:38 +0700

    Add files via upload

---
+++ cau1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8; //
+        Console.Write("Nhập vào một số: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine("Số {0} là số dương.", number);
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("Số {0} là số âm.", number);
+        }
+        else
+        {
+            Console.WriteLine("Số {0} là số 0.", number);
+        }
+    }
+}
